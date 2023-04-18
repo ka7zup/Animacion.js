6e17a05261cad39f3097abc147bca506b874476c
ka7zup <130498487+ka7zup@users.noreply.github.com>
2023-04-18 00:58:53 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', () => {
+    // Acciones a realizar al hacer clic en el botón
+    // En este ejemplo, no se realiza ninguna acción
+});
